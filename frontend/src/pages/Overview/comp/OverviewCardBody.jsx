40319df4ae85e6d7312849462a94bcb739dc9f20
gamitej <gamitejpratapsingh998@gmail.com>
2023-06-24 11:52:59 +0530

    height changed

--- frontend/src/pages/Overview/comp/OverviewCardBody.jsx
+++ frontend/src/pages/Overview/comp/OverviewCardBody.jsx
@@ -37,7 +37,7 @@ const OverviewCardBody = ({
   };
 
   return (
-    <div id="hideScrollBar" className="overflow-auto h-[calc(20rem-4rem)]">
+    <div id="hideScrollBar" className="overflow-auto h-[calc(22rem-4rem)]">
       {cardBodyData?.map(({ name, url, platform, solved, id }) => (
         <React.Fragment key={id}>
           <div className="grid grid-cols-8 p-4 hover:bg-slate-100 cursor-pointer">
